ae0cbfab7fa0f723b3411bf45838eb6eaa69ad81
Ethantran2002 <e.tran1117@gmail.com>
2023-03-13 21:06:09 -0600

    added for loop to generate password until it satisfies # of characters

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+document.querySelector("#generate").addEventListener("click", function(writePassword){
 
 var lowercaseAlpha = ("abcdefghijklmnopqrstuvwxyz")
 var uppercaseAlpha = ("ABCDEGHIJKLMNOPQRSTUVWXYZ")
@@ -15,7 +15,10 @@ var passLength = prompt("How many characters for your password? Pick a number be
 
 if (passLength < 8 || passLength > 128) {
   alert("Please choose a number between 8 and 128.")
+  return;
 }
+
+//charcter types
 
 var requireLowercase = confirm("Do you want your password to contain lowercase characters?")
 var requireUppercase = confirm("Do you want your password to contain uppercase characters?")
@@ -24,24 +27,48 @@ var requireSpecial = confirm("Do you want your password to contain special chara
 
 //testing that the user's password requirements meets the satisfactory requirements of a password.
 
-var charCount = 0
-
 if (requireLowercase !== true && requireUppercase !== true && requireNumbers !== true && requireSpecial !== true) {
   alert("Please select at least one character type.")
   return;
 }
 
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  
+  for (let i = 0; i < passLength; i++) {
+    
+    password = password + charTypes.charAt(Math.floor(Math.random() * Math.floor(charTypes.length - 1)));
+    
+    passwordText.value = password;
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (requireLowercase && !requireUppercase && !requireNumbers && !requireSpecial) {
+  charTypes = lowercaseAlpha;
+  generatePassword();
+
+} 
+else if (requireLowercase && requireUppercase && !requireNumbers && !requireSpecial) {
+  charTypes = lowercaseAlpha + uppercaseAlpha;
+} 
+else if (requireLowercase && requireUppercase && requireNumbers && !requireSpecial) {
+  charTypes = lowercaseAlpha + uppercaseAlpha + numChars;
+}
+else if (requireLowercase && requireUppercase && requireNumbers && requireSpecial) {
+  charTypes = lowercaseAlpha + uppercaseAlpha + numChars + specialChars;
+}
+else if (!requireLowercase && requireUppercase && !requireNumbers && !requireSpecial) {
+  charTypes = uppercaseAlpha;
+} 
+else if (!requireLowercase && requireUppercase && requireNumbers && !requireSpecial) {
+  charTypes = uppercaseAlpha + numChars;
+}
+else
+  
+
+
+
+
+})
